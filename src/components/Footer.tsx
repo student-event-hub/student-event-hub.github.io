4e5a35b076ad33e6a361d997290e161130cd7832
Ahron Natividad <ahron.natividad12@gmail.com>
2026-04-15 12:38:52 -1000

    updated landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,16 @@ import { Col, Container } from 'react-bootstrap';
 
 /* The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
-  <footer className="footer mt-auto py-3 bg-dark">
+  <footer className="footer mt-auto py-3 landing-blue-background">
     <Container>
-      <Col className="text-center" style={{ color: 'white' }}>
+      <Col className="text-center" style={{ color: '#FFF2F2' }}>
         The Bowfolios Project
         <br />
         University of Hawaii
         <br />
         Honolulu, HI 96822
         <br />
-        <a style={{ color: 'white' }} href="https://bowfolios.github.io">
+        <a style={{ color: '#FFF2F2' }} href="https://bowfolios.github.io">
           https://bowfolios.github.io
         </a>
       </Col>
