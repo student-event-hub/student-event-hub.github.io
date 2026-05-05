0344e9de9c0e1d5bcbd4be7850177d927a7a00e2
Ahron Natividad <ahron.natividad12@gmail.com>
2026-05-05 10:48:08 -1000

    Updated Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,98 @@
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import { PageIDs } from '@/utilities/ids';
+import { Facebook, Instagram, Link45deg } from 'react-bootstrap-icons';
 
 export default function Home() {
   return (
     <main>
       <div id={PageIDs.landingPage}>
-        <Row className="landing-blue-background">
-          <Col>
-            <Container className="text-center">
-              <h1
-                style={{ paddingTop: '20px', color: '#FFF2F2', fontSize: '36pt' }}
+        <Row className="landing-blue-background" style={{ alignItems: 'stretch' }}>
+          <Col className="d-flex flex-column">
+            <Container className="text-center d-flex flex-column h-100" style={{ paddingTop: '40px' }}>
+              <h1 style={{
+                paddingLeft: '20px',
+                paddingBottom: '50px',
+                color: '#FFF2F2',
+                fontSize: '36pt',
+              }}
               >
-                Welcome to the Student Event Hub
+                A Digitized, Centralized Bulletin Board for the UH Community
               </h1>
-              <h3 style={{ paddingBottom: '20px', color: '#FFF2F2' }}>
-                A digitized, centralized bulletin board for the UH Community
-              </h3>
+
+              <h1 style={{ color: '#FFF2F2' }}>How it Works</h1>
+              <ul className="list-group list-group-flush justify-content-center align-items-center">
+                <li className="list-group-item landing-list-item-blue">
+                  Browse and filter UH related events and activities
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  Save events to your personal calendar
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  Rate, comment, and submit your own events
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  Admin panel to manage events and view user engagement
+                </li>
+              </ul>
+              <div className="d-flex gap-5 justify-content-center mt-4 w-100">
+                <Button variant="light" href="/events" size="lg" style={{ minWidth: '160px' }}>
+                  Browse All Events
+                </Button>
+              </div>
+              <div className="mt-auto pb-5 d-flex justify-content-center gap-5">
+                <a href="https://www.instagram.com/sldatuhm/" target="_blank" rel="noreferrer">
+                  <Instagram size={28} color="white" className="mx-1" />
+                </a>
+                <a href="https://www.facebook.com/uhm.studentlife" target="_blank" rel="noreferrer">
+                  <Facebook size={28} color="white" className="mx-1" />
+                </a>
+                <a href="https://manoa.hawaii.edu/studentlife/" target="_blank" rel="noreferrer">
+                  <Link45deg size={28} color="white" className="mx-1" />
+                </a>
+              </div>
             </Container>
           </Col>
-          <Col>
-            <div className="landing-blue-background">
-              <Container className="text-center">
-                <h1 style={{ color: '#FFF2F2' }}>How it Works</h1>
-                <ul className="list-group list-group-flush justify-content-center align-items-center">
-                  <li className="list-group-item landing-list-item-blue">
-                    Browse and filter UH related events and activities
-                  </li>
-                  <li className="list-group-item landing-list-item-blue">
-                    Save events to your personal calendar
-                  </li>
-                  <li className="list-group-item landing-list-item-blue">
-                    Rate, comment, and submit your own events
-                  </li>
-                </ul>
-              </Container>
-            </div>
+          <Col className="landing-image-col" style={{ padding: 0, position: 'relative', overflow: 'hidden' }}>
+            {/* Blurred background fill */}
+            <div style={{
+              position: 'absolute',
+              inset: 0,
+              backgroundImage: 'url(/images/UH_Manoa.jpeg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              filter: 'blur(12px)',
+              transform: 'scale(1.15)',
+            }}
+            />
+            {/* Full image on top, fully visible */}
+            <Image
+              src="/images/UH_Manoa.jpeg"
+              alt="UH Manoa campus"
+              style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                display: 'block',
+              }}
+            />
           </Col>
         </Row>
+
+        { /* Notable Features */ }
         <Row>
           <div className="landing-white-background text-center">
             <h1 style={{ color: '#2D336B' }}>
               Notable Features
             </h1>
-            <Container>
+            <Container className="d-flex flex-column align-items-center gap-5">
               <Row md={1} lg={2}>
                 <Col xs={6}>
                   <Image
-                    src="/images/interests-page.png"
-                    width={500}
-                    alt="interest-page"
-                  />
-                </Col>
-                <Col xs={6}>
-                  <Image
-                    src="/images/filter-page.png"
-                    width={580}
-                    alt="filter-page"
+                    src="/images/All_Events_Page.png"
+                    width={1000}
+                    alt="all-events-page"
+                    className="rounded shadow-lg"
                   />
                 </Col>
               </Row>
@@ -73,9 +110,6 @@ export default function Home() {
                     <li className="list-group-item landing-list-item-white">
                       Users can like or dislike events and use the comment button with all comments sections.
                     </li>
-                    <li className="list-group-item landing-list-item-white">
-                      Page Admins will be able to see users that added events to their calendars.
-                    </li>
                   </ul>
                 </Col>
                 <Col xs={6}>
@@ -83,10 +117,7 @@ export default function Home() {
                     className="list-group list-group-flush justify-content-center align-items-center"
                   >
                     <li className="list-group-item landing-list-item-white">
-                      Admins have the ability to DELETE events.
-                    </li>
-                    <li className="list-group-item landing-list-item-white">
-                      Users can add events to their calendar.
+                      Users can add other people&apos;s events to their calendar.
                     </li>
                     <li className="list-group-item landing-list-item-white">
                       Being able to find future and current events
@@ -100,6 +131,75 @@ export default function Home() {
               </Row>
             </Container>
           </div>
+        </Row>
+
+        { /* Admin Panel Description */}
+        <Row className="landing-blue-background" style={{ alignItems: 'stretch' }}>
+          <Col className="landing-image-col" style={{ padding: 0, position: 'relative', overflow: 'hidden' }}>
+            {/* Blurred background fill */}
+            <div style={{
+              position: 'absolute',
+              inset: 0,
+              backgroundImage: 'url(/images/UH_Manoa.jpeg)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              filter: 'blur(12px)',
+              transform: 'scale(1.15)',
+            }}
+            />
+            {/* Full image on top, fully visible */}
+            <Image
+              src="/images/Admin_Page.png"
+              alt="UH Manoa campus"
+              style={{
+                position: 'relative',
+                width: '100%',
+                height: '100%',
+                objectFit: 'contain',
+                display: 'block',
+              }}
+            />
+          </Col>
+          <Col className="d-flex flex-column">
+            <Container className="text-center d-flex flex-column h-100" style={{ paddingTop: '40px' }}>
+              <h1 style={{
+                paddingLeft: '20px',
+                paddingBottom: '50px',
+                color: '#FFF2F2',
+                fontSize: '36pt',
+              }}
+              >
+                The Admin Panel
+              </h1>
+
+              <h1 style={{ color: '#FFF2F2' }}>For Web Admins and Moderators</h1>
+              <ul className="list-group list-group-flush justify-content-center align-items-center">
+                <li className="list-group-item landing-list-item-blue">
+                  Admins can view all events and user engagement metrics in one place
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  Have the ability to delete events that violate guidelines or are inappropriate
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  They can also view all comments and delete any that are inappropriate or violate guidelines
+                </li>
+                <li className="list-group-item landing-list-item-blue">
+                  Finally Edit events that have been submitted by users to ensure accuracy and appropriateness
+                </li>
+              </ul>
+              <Image
+                src="/images/Edit_Page.png"
+                alt="Edit Page"
+                style={{
+                  marginTop: '40px',
+                  width: '75%',
+                  height: 'auto',
+                  objectFit: 'contain',
+                }}
+                className="mx-auto d-block"
+              />
+            </Container>
+          </Col>
         </Row>
       </div>
     </main>
